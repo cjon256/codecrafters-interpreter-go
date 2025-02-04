a073c9f5011c83f556a2383844fb25d57dacf88f
Cullen J. ONeill <3659487+cjon256@users.noreply.github.com>
2025-02-04 00:14:50 -0500

    working on a new approach, currently incomplete

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -1,17 +1,98 @@
 package parser
 
 import (
+	"errors"
 	"fmt"
 
 	"example.com/cjon/tokenizer"
 )
 
+type ASTnode interface {
+	fmt.Stringer
+}
+
+type ASTgroup struct {
+	Contents ASTnode
+}
+
+func (g ASTgroup) String() string {
+	return fmt.Sprintf("(group %s)", g.Contents)
+}
+
+type ASTliteral struct {
+	Contents string
+}
+
+func (l ASTliteral) String() string {
+	return l.Contents
+}
+
+func ParseLines(tokens chan tokenizer.TokenStruct) error {
+}
+
 func Parse(tokens chan tokenizer.TokenStruct) error {
+	var group func() (ASTnode, error)
+	group = func() (ASTnode, error) {
+		g := ASTgroup{}
+		c := <-tokens
+		switch c.Type {
+		case tokenizer.EOF:
+			return g, errors.New("parse_error")
+		case tokenizer.RIGHT_PAREN:
+			return g, errors.New("parse_error")
+		case tokenizer.LEFT_PAREN:
+			var err error
+			g.Contents, err = group()
+			if err != nil {
+			}
+		case tokenizer.STRING:
+			g.Contents = ASTliteral{c.Literal}
+		case tokenizer.NUMBER:
+			g.Contents = ASTliteral{c.Literal}
+		case tokenizer.IDENTIFIER:
+			g.Contents = ASTliteral{c.Literal}
+		case tokenizer.TRUE:
+			g.Contents = ASTliteral{c.Literal}
+		case tokenizer.FALSE:
+			g.Contents = ASTliteral{c.Literal}
+		case tokenizer.NIL:
+			g.Contents = ASTliteral{c.Literal}
+
+		default:
+			return g, errors.New("parse_error")
+		}
+
+		close := <-tokens
+		if close.Type != tokenizer.LEFT_PAREN {
+			return g, errors.New("parse_error")
+		}
+		return g, nil
+	}
 	for t := range tokens {
-		if t.Literal == "null" {
-			fmt.Println(t.Lexeme)
-		} else {
-			fmt.Println(t.Literal)
+		switch t.Type {
+		case tokenizer.EOF:
+			continue
+		case tokenizer.RIGHT_PAREN:
+			return errors.New("parse_error")
+		case tokenizer.LEFT_PAREN:
+			err := group()
+			if err != nil {
+				return err
+			}
+		case tokenizer.STRING:
+			fmt.Print(t.Literal)
+		case tokenizer.NUMBER:
+			fmt.Print(t.Literal)
+		case tokenizer.IDENTIFIER:
+			fmt.Print(t.Lexeme)
+		case tokenizer.TRUE:
+			fmt.Print(t.Lexeme)
+		case tokenizer.FALSE:
+			fmt.Print(t.Lexeme)
+		case tokenizer.NIL:
+			fmt.Print(t.Lexeme)
+		default:
+			return errors.New("parse_error")
 		}
 	}
 	return nil
